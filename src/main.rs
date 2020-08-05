6efce2e80a2ac7f82d431f08de08a414d4098af8
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-08-06 01:31:01 +0200

    feat: read stdin for next version
    This change would allow for using piping to pass down the next version.
    A useful CLI combination would be `next-semver | cargo next`, allowing
    to automatically update the version according to semantic release
    commits.
    
    `next-semver` is a tool written by me (SirWindfield) as well and
    published under the @conventional-commits-rs organization on GitHub.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use pico_args::Arguments;
 use std::env::{args_os, current_dir};
 use std::fs;
+use std::io;
 use std::process::exit;
 use toml_edit::{value, Document};
 
@@ -17,17 +18,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args_to_use = args_os().skip(2).collect();
     let mut args = Arguments::from_vec(args_to_use);
 
-    let args = Args {
+    let mut args = Args {
         help: args.contains(["-h", "--help"]),
         version: args.contains(["-V", "--version"]),
         next_version: args.free()?,
     };
+
+    // Check standard-in to allow for piping.
+    let mut piped = String::new();
+    io::stdin().read_line(&mut piped)?;
+    let piped_trim = piped.trim();
+    if !piped_trim.is_empty() {
+        args.next_version.push(piped_trim.to_string());
+    }
 
     if args.help {
         println!("{}", HELP);
     } else if args.version {
         println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     } else if args.next_version.len() != 1 {
+        eprintln!(
+            "Only one version can be specified! Passed: {:?}",
+            args.next_version
+        );
         eprintln!("{}", HELP);
         exit(1);
     } else {
